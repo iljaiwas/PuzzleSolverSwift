eacd518821484c6a534aed2e3d6fc869e9b14d64
Ilja Iwas <2284114+iljaiwas@users.noreply.github.com>
2023-01-29 17:36:56 +0100

    Initial Commit

---
+++ PuzzleSolverSwift/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  PuzzleSolverSwift
+//
+//  Created by ilja on 29.01.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
